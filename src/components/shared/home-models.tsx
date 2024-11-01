3b6261ac25bee75cc02b1895591dc6f05d5f48ad
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-01 17:46:58 +0500

    new section process

---
+++ src/components/shared/home-models.tsx
@@ -0,0 +1,15 @@
+import Tabs from './tabs';
+
+const HomeModels = () => {
+  return (
+    <section className="pb-3 pt-[60px] flex flex-col gap-6">
+      <h1 className="text-center font-bold text-[56px] leading-[120%] text-[#1C1C1C]">
+        JETOUR MODELLERI
+      </h1>
+
+      <Tabs />
+    </section>
+  );
+};
+
+export default HomeModels;
