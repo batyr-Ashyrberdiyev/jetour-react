847b010c0b17fe8c034b94cfc2995b873e23b0e6
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-01 17:46:44 +0500

    scroll on active slide added

--- src/components/shared/bullets.tsx
+++ src/components/shared/bullets.tsx
@@ -1,25 +1,24 @@
-import { cn } from "../../lib/utils";
+import { cn } from '../../lib/utils';
 
 interface Props {
   size: number;
   current: number;
   className?: string;
+  onBullet: (i: number) => void;
 }
 
-const Bullets = ({ size, current = 0, className }: Props) => {
+const Bullets = ({ size, current = 0, className, onBullet }: Props) => {
   return (
-    <div className={cn(className, "flex items-center justify-center gap-6")}>
+    <div className={cn(className, 'flex items-center justify-center gap-6')}>
       {[...Array(size)].map((_, i) => (
         <div
           key={i}
+          onClick={() => onBullet(i)}
           className={cn(
-            "rounded-full flex justify-center cursor-pointer items-center size-5 p-[1px]",
-            current === i ? "bg-white" : "bg-white/50"
-          )}
-        >
-          {current === i && (
-            <div className="bg-[#242126] size-full rounded-full" />
-          )}
+            'rounded-full flex justify-center cursor-pointer items-center size-5 p-[1px]',
+            current === i ? 'bg-white' : 'bg-white/50',
+          )}>
+          {current === i && <div className="bg-[#242126] size-full rounded-full" />}
         </div>
       ))}
     </div>
