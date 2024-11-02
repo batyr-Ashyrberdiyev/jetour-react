b4876054f4b24b266163962efa45d326ddd83dbb
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-02 15:00:06 +0500

    new section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import HomeFaq from '../components/shared/home-faq';
 import HomeModels from '../components/shared/home-models';
 import HomeSlider from '../components/shared/home-slider';
 
@@ -6,6 +7,7 @@ const Home = () => {
     <main>
       <HomeSlider />
       <HomeModels />
+      <HomeFaq />
     </main>
   );
 };
