a3563d160ac40005070758b6f7d7425a41620515
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-07 19:00:33 +0500

    in process

---
+++ src/components/shared/home-benefits.tsx
@@ -0,0 +1,7 @@
+export const HomeBenefits = () => {
+  return (
+    <section className="bg-[#242126] py-6">
+      <div className="container flex flex-col gap-[26px]"></div>
+    </section>
+  );
+};
