0fcf6230c585e22705423dd0518813e1c05b43d9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-11-07 19:01:16 +0500

    finish

--- src/components/shared/home-partners.tsx
+++ src/components/shared/home-partners.tsx
@@ -1,26 +1,73 @@
-import { Slider } from "./slider";
+import Autoplay, { AutoplayType } from 'embla-carousel-autoplay';
+import { Carousel, CarouselApi, CarouselContent, CarouselItem } from '../ui/carousel';
+import { useEffect, useState } from 'react';
+import { SectionInfo } from './section-info';
 
 const partners = [
   {
-    logo: "/images/partners/soueast.png",
+    logo: '/images/partners/soueast.png',
   },
   {
-    logo: "/images/partners/jetour.svg",
+    logo: '/images/partners/jetour.svg',
   },
   {
-    logo: "/images/partners/petronas.svg",
+    logo: '/images/partners/petronas.svg',
+  },
+  {
+    logo: '/images/partners/soueast.png',
+  },
+  {
+    logo: '/images/partners/jetour.svg',
+  },
+  {
+    logo: '/images/partners/petronas.svg',
   },
 ];
 
 export const HomePartners = () => {
+  const [api, setApi] = useState<CarouselApi>();
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    const autoplay: AutoplayType = api.plugins().autoplay;
+
+    const handleSelect = () => {
+      autoplay.stop();
+      setTimeout(() => {
+        autoplay.play();
+      }, autoplay.options.delay);
+    };
+
+    api.on('select', handleSelect);
+
+    return () => {
+      api.off('select', handleSelect);
+    };
+  }, [api]);
+
   return (
     <section className="pt-[50px] font-bold pb-[72px]">
-      <div className="text-center">
-        <h1 className="text-[#1C1C1C] text-[40px] leading-[145%]">HYZMATDAŞ</h1>
-        <h4 className="text-[#B0B0B0] text-[18px] leading-[140%]">
-          Partnerlarymyz
-        </h4>
-      </div>
+      <SectionInfo title={'HYZMATDAŞ'} subtitle={'Partnerlarymyz'} />
+
+      <Carousel
+        setApi={setApi}
+        opts={{ loop: true, duration: 7000, dragThreshold: 1000, watchDrag: false }}
+        plugins={[
+          Autoplay({
+            delay: 0,
+          }),
+        ]}>
+        <CarouselContent className="h-20">
+          {partners.map((item, i) => (
+            <CarouselItem className="basis-1/4 h-full">
+              <img key={i} src={item.logo} alt="logo" className="size-full object-contain" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </section>
   );
 };
